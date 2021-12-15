5489882d0086579c3b98e49ab5ce80e8cb55c75a
AshFoster <ashfoster87@gmail.com>
2021-12-15 08:20:54 +0000

    Add tests for add turn function

--- scripts/game.js
+++ scripts/game.js
@@ -10,11 +10,18 @@ function newGame() {
     game.currentGame = [];
     game.playerMoves = [];
     showScore();
+    addTurn();
 }
 
 function showScore() {
     document.getElementById("score").innerText = game.score;
 }
 
+function addTurn() {
+    game.playerMoves = [];
+    game.currentGame.push(game.choices[(Math.floor(Math.random() * 4))]);
+    // showTurns();
+}
 
-module.exports = { game, newGame, showScore };
+
+module.exports = { game, newGame, showScore, addTurn };
--- scripts/tests/game.test.js
+++ scripts/tests/game.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-const { game, newGame, showScore } = require("../game");
+const { game, newGame, showScore, addTurn } = require("../game");
 
 beforeAll(() => {
 
@@ -42,8 +42,8 @@ describe("newGame works correctly", () => {
     test("should set game score to zero", () => {
         expect(game.score).toEqual(0);
     });
-    test("should clear current game array", () => {
-        expect(game.currentGame.length).toBe(0);
+    test("should be one move in computer's game array", () => {
+        expect(game.currentGame.length).toBe(1);
     })
     test("should clear player moves array", () => {
         expect(game.playerMoves.length).toBe(0);
